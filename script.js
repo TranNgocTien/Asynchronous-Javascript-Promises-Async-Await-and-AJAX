40ee5eeaf60d072a49205a8b87ea19ba08e40384
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-04 19:58:36 +0700

    Asynchronous Javascript, AJAX and APIs

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const p = document.querySelector('.p');
+p.textContent='My name is Jonas!';
+alert('Text set!');
+p.style.color='red';
+
+const p=document.querySelector('.p');
+setTimeout(function(){
+    p.textContent='My name is Jonas!';
+},5000);
+p.style.color='red';
+
+const img = document.querySelector('.dog');
+img.src= 'dog.jpg';
+img.addEventListener('load',function(){
+    img.classList.add('fadeIn');
+});
+package.style.width='300px';
+
