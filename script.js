f69e4459f9975c04ea573cf3ae5abbcace900add
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-06 19:13:13 +0700

    Callback hell

--- script.js
+++ script.js
@@ -21,32 +21,63 @@ const countriesContainer = document.querySelector('.countries');
 //     img.classList.add('fadeIn');
 // });
 // package.style.width='300px';
-
-const getCountryData= function(country){
-const request = new XMLHttpRequest();
-request.open('GET',`https://restcountries.com/v2/name/${country}`);
-request.send()
-request.addEventListener('load',function(){
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
+const renderCountry = function(data, className=''){
     const html=`
-            <article class="country">
-                <img class="country__img" src="${data.flags.svg}" />
-                <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
-                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-                </div>
-             </article>
-    `;
-    countriesContainer.insertAdjacentHTML('beforeend',html);
-    countriesContainer.style.opacity=1;
-});
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+     </article>
+`;
+countriesContainer.insertAdjacentHTML('beforeend',html);
+countriesContainer.style.opacity=1;
 }
 
-getCountryData(`portugal`);
-getCountryData('usa');
-getCountryData('vietnam');
+const getCountryAndNeighbour= function(country){
+const request = new XMLHttpRequest();
+request.open('GET',`https://restcountries.com/v2/name/${country}`);
+request.send();
+request.addEventListener('load',function(){
+    //AJAX call country 1
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    
+    //Render country 1
+    renderCountry(data);
+
+    //Get neighbour country (2)
+    const [neighbour] = data.borders;
+    if(!neighbour) return;
+    
+
+    //AJAX call country 2
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET',`https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function(){
+        const data2 =JSON.parse(this.responseText);
+        renderCountry(data2, 'neighbour');
+    });
+});
+};
+
+getCountryAndNeighbour(`portugal`);
+
+
+// setTimeout(()=>{
+//     console.log('1 second passed');
+//     setTimeout(()=>{
+//         console.log('2 second passed');
+//         setTimeout(()=>{
+//             console.log('3 second passed');
+//         })
+//     },1000)
+// },1000)
+
