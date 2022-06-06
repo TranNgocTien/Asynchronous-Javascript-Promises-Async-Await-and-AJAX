f58d9c3c0af3e55abb7ed39474165872e5c4d661
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-06 20:28:29 +0700

    Promises and the Fetch API

--- script.js
+++ script.js
@@ -81,3 +81,9 @@ getCountryAndNeighbour(`portugal`);
 //     },1000)
 // },1000)
 
+
+// const request = new XMLHttpRequest();
+// request.open('GET',`https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+const request=fetch('https://restcountries.com/v2/name/portugal');
