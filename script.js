396e6b72693656e81db689c902e777fb7f9ae59b
TranNgocTien <58336167+TranNgocTien@users.noreply.github.com>
2022-06-11 23:39:09 +0700

    coding challenge #2

--- script.js
+++ script.js
@@ -135,29 +135,29 @@ const getJSON = function(url, msg="Something went wrong"){
 // }
 
 
-const getCountryData = function(country){
-    //country 1
-    getJSON(`https://restcountries.com/v2/name/${country}`,'Country not found')
-    .then((data)=>{
+// const getCountryData = function(country){
+//     //country 1
+//     getJSON(`https://restcountries.com/v2/name/${country}`,'Country not found')
+//     .then((data)=>{
         
-        renderCountry(data[0]);
-        const neighbour = data[0].borders?.[0];
+//         renderCountry(data[0]);
+//         const neighbour = data[0].borders?.[0];
         
-        if(!neighbour) throw new Error('No neighbour found!');
-        //country 2
-        return getJSON(`https://restcountries.com/v2/alpha/${neighbour},'Country not found'`);
-    })
-    .then(data=>renderCountry(data, "neighbour"))
-    .catch(err=>{
-        console.error(`${err} 💥💥💥`);
-        renderError(`Something went wrong 💥💥 ${err.message}. Try again!`)
-    })
-    .finally(()=> countriesContainer.style.opacity=1 );
-}
+//         if(!neighbour) throw new Error('No neighbour found!');
+//         //country 2
+//         return getJSON(`https://restcountries.com/v2/alpha/${neighbour},'Country not found'`);
+//     })
+//     .then(data=>renderCountry(data, "neighbour"))
+//     .catch(err=>{
+//         console.error(`${err} 💥💥💥`);
+//         renderError(`Something went wrong 💥💥 ${err.message}. Try again!`)
+//     })
+//     .finally(()=> countriesContainer.style.opacity=1 );
+// }
 
-btn.addEventListener('click',function(){
-    getCountryData('vietnam');
-});
+// btn.addEventListener('click',function(){
+//     getCountryData('vietnam');
+// });
 
 
 
@@ -288,3 +288,62 @@ const whereAmI = function () {
   };
 
   btn.addEventListener('click', whereAmI);
+
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+const wait = function(seconds){
+    return new Promise(function(resolve){
+        setTimeout(resolve,seconds*1000);
+    })
+}
+const imgContainer=document.querySelector('.images');
+
+const createImage = function(imgPath){
+    return new Promise(function(resolve,reject){
+        const  img= document.createElement('img');
+        img.src=imgPath;
+
+        img.addEventListener('load',function(){
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error',function(){
+            reject(new Error('Image not found'));
+        })
+    });
+}
+
+let currentImg;
+
+createImage('img/img-1.jpg').then(img=>{
+    console.log('Image 1 loaded');
+    currentImg=img;
+    return wait(2);
+}).then(()=>{
+    currentImg.style.display='none';
+    return createImage('img/img-2.jpg');
+}).then((img)=>{
+    currentImg=img;
+    return wait(2);
+}).then(()=>currentImg.style.dislay='none').catch(err=>console.error(err));
